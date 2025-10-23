887b54cb3692d3e9db02c2df3ca80a56539a0beb
Edward Wayne Cruz <guaine86@gmail.com>
2025-10-23 20:50:13 -0300

    version-1.2

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,12 @@
 let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
+let puntosJugador = 0;
+
+// Referencias al HTML
+const btnPedir = document.querySelector('#btnPedir');
+const mostrarPuntos = document.querySelectorAll('small');
+const divCartasJugador = document.querySelector('#jugador-cartas');
 
 // Funcion para crear el mazo
 const crearDeck = ()=>{
@@ -28,5 +34,55 @@ const crearDeck = ()=>{
     // console.log(deck);
     return deck;
 }
-
 crearDeck();
+
+// Funcion para pedir una carta
+const pedirCarta = () => {
+
+    if(deck.length === 0){
+        throw "No hay mas cartas en el mazo!!";
+    }
+    
+    const carta = deck.pop();
+    return carta;
+}
+
+// Funcion para determinar el valor de la carta
+const valorCarta = (carta)=>{
+    const valor = carta.substring(0, carta.length - 1);
+    // let puntos = 0;
+
+    // if( isNaN(valor) ){
+    //     puntos = (valor === 'A') ? 11 : 10;
+        
+    // }else{
+    //     puntos = valor * 1;
+    // }
+
+    return ( isNaN(valor) ) ? 
+                    valor === 'A' ? 11 : 10
+                    : valor * 1;
+ }
+
+// Eventos
+btnPedir.addEventListener('click', ()=>{
+    const carta = pedirCarta();
+    puntosJugador += valorCarta(carta);
+    mostrarPuntos[0].innerText = puntosJugador;
+
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/${carta}.png`;
+    imgCarta.classList.add('carta');
+
+    divCartasJugador.appendChild(imgCarta);
+
+    if(puntosJugador > 21){
+        btnPedir.disabled = true;
+        console.warn("Lo siento mucho peeero...PERDISTE!!");
+    }else if(puntosJugador === 21){
+        btnPedir.disabled = true;
+        console.info("21!!!...GENIAL!!");
+    }
+
+    
+}) 
